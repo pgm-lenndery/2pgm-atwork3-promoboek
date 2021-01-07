27ef310418a4207f313df4cbf2a5f8c970c66f07
Jesse Vander <jessvand10@student.arteveldehs.be>
2021-01-07 22:10:37 +0100

    apollo client setup with rates

--- src/components/FloatCard/index.js
+++ src/components/FloatCard/index.js
@@ -3,9 +3,29 @@ import PurpleRain from '../PurpleRain';
 import { ArrowRight } from 'react-feather';
 import { Link } from 'react-router-dom';
 
+import { useQuery, gql } from '@apollo/client';
+
 import './index.scss';
 
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "EUR") {
+      currency
+      rate
+    }
+  }
+`;
+
 export default ({ children: content, image, title }) => {
+
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if(loading) return 'Loading products...';
+  if(error) return `Error: ${error.message}`;
+
+  const usdRate = data.rates[183];
+
+
     return (
         <Link to="/projecten" className="floatCard">
             <div className="floatCard__head">
@@ -19,6 +39,7 @@ export default ({ children: content, image, title }) => {
                 <div className="floatCard__body-overlay">
                     <h2>{ title }</h2>
                     <p>{ content }</p>
+                    <p>1 { usdRate.currency } = € { usdRate.rate }</p>
                 </div>
                 <PurpleRain>
                     <img src="https://pgmgent-1920-students.github.io/case1-pgm-website-baas-pgm-lenndery/src/images/cases/tronald%20dump/thumb.png" alt=""/>
